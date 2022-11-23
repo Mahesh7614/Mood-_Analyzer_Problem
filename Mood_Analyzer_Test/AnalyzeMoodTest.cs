0c7af5d8392132a58215eb254ba9a8a54204a15d
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-23 22:55:53 +0530

    Return Sad

--- Mood_Analyzer_Test/AnalyzeMoodTest.cs
+++ Mood_Analyzer_Test/AnalyzeMoodTest.cs
@@ -5,12 +5,20 @@ namespace Mood_Analyzer_Test
     public class AnalyzeMoodTest
     {
         [TestMethod]
-        public void Given_HappyOrSad_return_HappyOrSad()
+        public void Given_HappyOrSad_Return_HappyOrSad()
         {
             Mood_Analyzer mood = new Mood_Analyzer();
             string result = mood.AnalyzeMood("Happy");
 
             Assert.AreEqual("Happy".ToUpper(), result);
+        }
+        [TestMethod]
+        public void Given_Sad_Return_Sad()
+        {
+            Mood_Analyzer mood = new Mood_Analyzer();
+            string result = mood.AnalyzeMood("SAD");
+
+            Assert.AreEqual("Sad".ToUpper(), result);
         }
     }
 }
